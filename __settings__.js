46860e83f44f9eba6a04b1d6fae2009c5bcde8ae
pixelandboard <109473043+pixelandboard@users.noreply.github.com>
2022-08-13 19:13:16 +0800

    Add files via upload

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1474463.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 93714210, 93714211, 93714212, 93714213 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/93714234/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/93714235/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/93714233/1/ammo.js', 'preload' : true},
+];
